9c4052d55917b46986188dd8e7ae0f84648a1f9d
shinku_momoiro <shinku141.ai@gmail.com>
2020-10-27 12:00:03 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 'use strict';
 
+const memo = new Map();
+// 固定化されているフィボナッチ数列を代入
+memo.set(0, 0);
+memo.set(1, 1);
+
 function fib(n) {
-    if (n === 0){
-        return 0;
-    } else if (n === 1){
-        return 1;
+    // 既に計算済みのフィボナッチ数列を返す
+    if (memo.has(n)){
+        return memo.get(n);
     }
-    return fib(n - 1) + fib(n - 2);
+    // 計算、記録、返答
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 
 const length = 40;
